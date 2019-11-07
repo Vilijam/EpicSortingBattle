41e2e1b88817213e5ad40513b52f3780d82355fd
Vilijam <37106752+Vilijam@users.noreply.github.com>
2019-11-07 10:04:00 +0100

    begyndt insertionSort

---
+++ sorteringsAlgoritmer.py
@@ -0,0 +1,30 @@
+import matplotlib
+
+def insertionSort(list):
+
+    listSorted = list
+
+    for i in range(0,len(listSorted) - 1)
+        if i < i-1
+            for
+        
+        
+
+
+
+    pass
+
+
+def selectionSort(list):
+
+    pass
+
+
+def bubbleSort(list):
+
+    pass
+
+
+def mergeSort(list):
+
+    pass
